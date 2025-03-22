2912eb05376b1081e5df6533d442862afc769d49
Jai Gawande <gawande20jai@gmail.com>
2025-03-22 20:41:29 +0530

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,24 @@ const { DataSource } = require('typeorm');
 
 const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'localhost',
+    host: 'sql12.freesqldatabase.com',
     port: 3306,
-    username: 'root',
-    password: 'krsna$02',
-    database: 'ecommerce_db',
+    username: 'sql12768986',
+    password: 'AgyyeC9mBG',
+    database: 'sql12768986',
     entities: [require('../models/Product')],
     synchronize: true, // Auto-create tables
 });
+// const AppDataSource = new DataSource({
+//     type: 'mysql',
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'root',
+//     password: 'krsna$02',
+//     database: 'ecommerce_db',
+//     entities: [require('../models/Product')],
+//     synchronize: true, // Auto-create tables
+// });
 
 AppDataSource.initialize()
     .then(() => console.log('Database connected'))
